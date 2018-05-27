Transferred more parameters to the Parameters file.  Fixed error in handling of batch normalization (train vs test).

dataset.py
@@ -14,7 +14,7 @@ def decode(serialized_example):
     print(features)
 
     image = tf.decode_raw(features['image'], tf.float32)    
-    image = tf.reshape(image, [224, 224, 3])
+    image = tf.reshape(image, [Parameters.IMAGE_SIZE, Parameters.IMAGE_SIZE, 3])
 
     label = tf.cast(features['expected_output'], tf.float32)
     #label = tf.one_hot(label, 2, 1.0, 0.0) #Convert from integer labels to one hot representation

train.py
@@ -17,20 +17,17 @@ seed(1)
 from tensorflow import set_random_seed
 set_random_seed(2)
 
-batch_size = 16
-
 #Prepare input data
-img_size = 224
 num_channels = 3
 
 def GetHeInitializer():
     return tf.contrib.layers.variance_scaling_initializer()
 
-def AddResidualModule(inputVariable, outputDepth, stride):
+def AddResidualModule(inputVariable, outputDepth, stride, isTraining):
     conv1 = tf.layers.conv2d(inputs=inputVariable, filters=outputDepth, kernel_size=[3, 3], padding="same", activation=tf.nn.relu, kernel_initializer=GetHeInitializer(), strides=(stride, stride))
-    batchnorm1 = tf.layers.batch_normalization(conv1)
+    batchnorm1 = tf.layers.batch_normalization(conv1, training=isTraining)
     conv2 = tf.layers.conv2d(inputs=batchnorm1, filters=outputDepth, kernel_size=[3, 3], padding="same", activation=tf.nn.relu, kernel_initializer=GetHeInitializer())
-    batchnorm2 = tf.layers.batch_normalization(conv2)
+    batchnorm2 = tf.layers.batch_normalization(conv2, training=isTraining)
 
     if (stride != 1) or (inputVariable.get_shape()[3] != outputDepth):
         skip_conv = tf.layers.conv2d(inputs=inputVariable, filters=outputDepth, kernel_size=[1, 1], padding="same", strides=(stride, stride), activation=tf.nn.relu, kernel_initializer=GetHeInitializer())
@@ -43,7 +40,7 @@ def AddResidualModule(inputVariable, outputDepth, stride):
         return relu_sum
 
 
-def ConstructNetwork(imageSize, numberOfChannels, numberOfOutputs):
+def ConstructNetwork(imageSize, numberOfChannels, numberOfOutputs, isTraining):
     input_placeholder = tf.placeholder(tf.float32, shape=[None, imageSize, imageSize, numberOfChannels], name='input')
 
     #Labels
@@ -51,38 +48,42 @@ def ConstructNetwork(imageSize, numberOfChannels, numberOfOutputs):
 
     layers = []
 
-    with tf.variable_scope('conv0'):
+    with tf.variable_scope('conv0', reuse=tf.AUTO_REUSE):
         conv0 = tf.layers.conv2d(inputs=input_placeholder, filters=64, kernel_size=[7, 7], padding="same", activation=tf.nn.relu, kernel_initializer=GetHeInitializer(), strides=(2,2))
         layers.append(conv0)
 
-    with tf.variable_scope('maxpool0'):
+    with tf.variable_scope('maxpool0', reuse=tf.AUTO_REUSE):
         maxpool0 = tf.layers.max_pooling2d(inputs=layers[-1], pool_size=[2,2], strides=2)
         layers.append(maxpool0)
 
-    network_head = AddResidualModule(layers[-1], 64, 1)
+    with tf.variable_scope("res0_", reuse=tf.AUTO_REUSE):
+        network_head = AddResidualModule(layers[-1], 64, 1, isTraining)
 
     network_parameters = [[64, 1], [64, 1], [64, 1], [128, 2], [128, 1], [128, 1], [128, 1], [256, 2], [256, 1], [256, 1], [256, 1], [256, 1], [512, 2], [512, 1], [512, 1]]
 
-    for module_parameters in network_parameters:
-        next_module = AddResidualModule(layers[-1], module_parameters[0], module_parameters[1])
-        layers.append(next_module)
+    for index in range(0, len(network_parameters)):
+            with tf.variable_scope("res" + str(index) + "_", reuse=tf.AUTO_REUSE):
+                module_parameters = network_parameters[index]
+                next_module = AddResidualModule(layers[-1], module_parameters[0], module_parameters[1], isTraining)
+                layers.append(next_module)
 
-    flat = tf.reshape(layers[-1], [-1, layers[-1].get_shape()[1:4].num_elements()])
-    fc0 = tf.layers.dense(inputs=flat, units=numberOfOutputs, kernel_initializer=GetHeInitializer())
+    with tf.variable_scope("res_fc_", reuse=tf.AUTO_REUSE):
+        flat = tf.reshape(layers[-1], [-1, layers[-1].get_shape()[1:4].num_elements()])
+        fc0 = tf.layers.dense(inputs=flat, units=numberOfOutputs, kernel_initializer=GetHeInitializer())
 
-    loss = tf.reduce_mean(tf.squared_difference(fc0, y_true))
+        loss = tf.reduce_mean(tf.squared_difference(fc0, y_true))
 
-    return input_placeholder, y_true, fc0, loss
+        return input_placeholder, y_true, fc0, loss
 
 def AddOptimizer(fc_network_output_op, loss_operator):
 
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     
     with tf.control_dependencies(update_ops):
-        optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss_operator)
+        optimizer = tf.train.AdamOptimizer(learning_rate=Parameters.INITIAL_TRAINING_RATE).minimize(loss_operator)
         return optimizer
 
-def ComputeLoss(lossOp, dataSetIterator, session):
+def ComputeLoss(lossOp, dataSetIterator, inputVar, labelVar, session):
     epoch_count = 0
     loss_sum = 0.0
     session.run(dataSetIterator.initializer)
@@ -92,7 +93,7 @@ def ComputeLoss(lossOp, dataSetIterator, session):
        try:
            images, labels = session.run([images_op, labels_op])
 
-           loss_sum += session.run(lossOp, feed_dict={x: images, y_true: labels})
+           loss_sum += session.run(lossOp, feed_dict={inputVar: images, labelVar: labels})
            epoch_count += 1
        except tf.errors.OutOfRangeError:
            break
@@ -100,15 +101,15 @@ def ComputeLoss(lossOp, dataSetIterator, session):
     loss = loss_sum/epoch_count
     return loss
 
-def ReportValidationLoss(lossOp, dataSetIterator, epoch, session):
-    validation_loss = ComputeLoss(lossOp, dataSetIterator, session)
+def ReportValidationLoss(lossOp, dataSetIterator, epoch, inputVar, labelVar, session):
+    validation_loss = ComputeLoss(lossOp, dataSetIterator, inputVar, labelVar, session)
 
     message = "Training Epoch {0} --- " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) +" --- Validation Loss: {1}"
     print(message.format(epoch, validation_loss))
     sys.stdout.flush()
     return validation_loss
 
-def SaveOutputsAsJson(fileName, outputOp, dataSetIterator, session):
+def SaveOutputsAsJson(fileName, outputOp, dataSetIterator, inputVar, labelVar, session):
     session.run(dataSetIterator.initializer)
     images_op, labels_op = dataSetIterator.get_next()
 
@@ -119,7 +120,7 @@ def SaveOutputsAsJson(fileName, outputOp, dataSetIterator, session):
        try:
            images, labels = session.run([images_op, labels_op])
 
-           output = session.run(outputOp, feed_dict={x: images, y_true: labels})
+           output = session.run(outputOp, feed_dict={inputVar: images, labelVar: labels})
 
            for i in range(0, output.shape[0]-1):
                base_name = "example_" + str(batch_number) + "_" + str(i) + "_";
@@ -137,7 +138,7 @@ def SaveOutputsAsJson(fileName, outputOp, dataSetIterator, session):
     json_file.close()
 
 
-def TrainForEpoch(trainOp, lossOp, dataSetIterator, session):
+def TrainForEpoch(trainOp, lossOp, dataSetIterator, inputVar, labelVar, session):
     number_of_iterations = 0
     loss_sum = 0.0
     session.run(dataSetIterator.initializer)
@@ -149,14 +150,14 @@ def TrainForEpoch(trainOp, lossOp, dataSetIterator, session):
         except tf.errors.OutOfRangeError:
             break
 
-        [_, batch_loss] = session.run([trainOp, lossOp], feed_dict={x: images, y_true: labels})
+        [_, batch_loss] = session.run([trainOp, lossOp], feed_dict={inputVar: images, labelVar: labels})
         number_of_iterations += 1
         loss_sum += batch_loss
     loss = loss_sum/number_of_iterations
 
     return number_of_iterations, loss
 
-def Train(numberOfEpochPerDataset, numberOfDatasets, checkpointPath, saver):
+def Train(numberOfEpochPerDataset, numberOfDatasets, checkpointPath, saver, trainingInput, trainingLabel, trainingOutput, trainingOp, trainingLoss, validationInput, validationLabel, validationOutput, validationLoss):
     training_log_file = open('trainingLog', 'w')
     training_log_file.write('Epoc, Training Loss, Validation Loss\n')
 
@@ -165,20 +166,20 @@ def Train(numberOfEpochPerDataset, numberOfDatasets, checkpointPath, saver):
         RegenerateTrainingData.RegenerateTrainingData()
 
         #Setup reading from tfrecords file
-        training_data_iterator = dataset.GetInputs(batch_size, 1, "/home/charlesrwest/cpp/Datasets/objectTransform/objectTransformDatasetTrain.tfrecords")
-        validation_data_iterator = dataset.GetInputs(batch_size, 1, "/home/charlesrwest/cpp/Datasets/objectTransform/objectTransformDatasetValidate.tfrecords")    
+        training_data_iterator = dataset.GetInputs(Parameters.BATCH_SIZE, 1, "/home/charlesrwest/cpp/Datasets/objectTransform/objectTransformDatasetTrain.tfrecords")
+        validation_data_iterator = dataset.GetInputs(Parameters.BATCH_SIZE, 1, "/home/charlesrwest/cpp/Datasets/objectTransform/objectTransformDatasetValidate.tfrecords")    
 
         for epoch in range(0, numberOfEpochPerDataset):
             #Training
-            [_, training_loss] = TrainForEpoch(optimizer, loss, training_data_iterator, session)
+            [_, training_loss] = TrainForEpoch(trainingOp, trainingLoss, training_data_iterator, trainingInput, trainingLabel, session)
             message = "Training Epoch {0} --- " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) +" --- Training Loss: {1}"
             print(message.format(epoch, training_loss))
             
             sys.stdout.flush()
 
             #Validation and reporting
-            validation_loss = ReportValidationLoss(loss, validation_data_iterator, epoch, session)
-            SaveOutputsAsJson("results/results"+ str(epoch) +".json", fc0, validation_data_iterator, session)
+            validation_loss = ReportValidationLoss(validationLoss, validation_data_iterator, epoch, validationInput, validationLabel, session)
+            SaveOutputsAsJson("results/results"+ str(epoch) +".json", validationOutput, validation_data_iterator, validationInput, validationLabel, session)
             message = "{0}, {1}, {2}\n"
             training_log_file.write(message.format(epoch, training_loss, validation_loss))
             training_log_file.flush()
@@ -192,12 +193,14 @@ session = tf.Session()
 
 
 #Make the network
-x, y_true, fc0, loss = ConstructNetwork(img_size, num_channels, Parameters.NUMBER_OF_NETWORK_OUTPUTS)
+training_input, training_label, training_output, training_loss = ConstructNetwork(Parameters.IMAGE_SIZE, num_channels, Parameters.NUMBER_OF_NETWORK_OUTPUTS, True)
+
+validation_input, validation_label, validation_output, validation_loss = ConstructNetwork(Parameters.IMAGE_SIZE, num_channels, Parameters.NUMBER_OF_NETWORK_OUTPUTS, False)
 
 session.run(tf.global_variables_initializer())
 
 #Add the optimizer
-optimizer = AddOptimizer(fc0, loss)
+optimizer = AddOptimizer(training_output, training_loss)
 
 session.run(tf.global_variables_initializer())
 
@@ -206,7 +209,7 @@ saver = tf.train.Saver()
 
 
 
-Train(1, 1000000, './object_transform-model', saver)
+Train(1, 1000000, './object_transform-model', saver, training_input, training_label, training_output, optimizer, training_loss, validation_input, validation_label, validation_output, validation_loss)
 
 
 
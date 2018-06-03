63d09d0af397c0de5de42b47fadec277fa54b8dc
Charles West <crwest@ncsu.edu>
2018-06-03 08:55:21 -0400

    Bad results with 1e-3 learning rate, so trying 5e-4.

--- Parameters.py
+++ Parameters.py
@@ -10,6 +10,6 @@ if ROTATION_TRACKING_ENABLED :
     NUMBER_OF_NETWORK_OUTPUTS += 6
 
 
-INITIAL_TRAINING_RATE = 1e-3
+INITIAL_TRAINING_RATE = 5e-4
 BATCH_SIZE = 64
 IMAGE_SIZE = 224 #Need to update image generation to set image size according to this parameter

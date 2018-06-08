07d4b2873e40b997e78cd8467a9eb8cf9dbbb5a7
Charles West <crwest@ncsu.edu>
2018-06-08 13:19:46 -0400

    Debugging example generation.

--- RegenerateTrainingData.py
+++ RegenerateTrainingData.py
@@ -15,8 +15,8 @@ def RegenerateTrainingData(outputFileName):
     RemoveFilesInFolder("/home/charlesrwest/storage/Datasets/objectTransform/rawData")
     
     devnull = open(os.devnull, 'w')
-    call(["/home/charlesrwest/cpp/libraries/blender-2.80-beta/blender", "/home/charlesrwest/cpp/projects/TensorFlow/objectTransform/catRepositionedTest.blend", "--background", "--python", "/home/charlesrwest/cpp/projects/TensorFlow/objectTransform/GenerateExampleData.py"], stdout=devnull, stderr=devnull)
-#    call(["/home/charlesrwest/cpp/libraries/blender-2.80-beta/blender", "/home/charlesrwest/cpp/projects/TensorFlow/objectTransform/catRepositionedTest.blend", "--background", "--python", "/home/charlesrwest/cpp/projects/TensorFlow/objectTransform/GenerateExampleData.py"])
+#    call(["/home/charlesrwest/cpp/libraries/blender-2.80-beta/blender", "/home/charlesrwest/cpp/projects/TensorFlow/objectTransform/catRepositionedTest.blend", "--background", "--python", "/home/charlesrwest/cpp/projects/TensorFlow/objectTransform/GenerateExampleData.py"], stdout=devnull, stderr=devnull)
+    call(["/home/charlesrwest/cpp/libraries/blender-2.80-beta/blender", "/home/charlesrwest/cpp/projects/TensorFlow/objectTransform/catRepositionedTest.blend", "--background", "--python", "/home/charlesrwest/cpp/projects/TensorFlow/objectTransform/GenerateExampleData.py"])
 
     convertToTfRecord.ConvertToTfRecord("/home/charlesrwest/storage/Datasets/objectTransform/rawData", "/home/charlesrwest/storage/Datasets/objectTransform/" + outputFileName, Parameters.IMAGE_SIZE, Parameters.IMAGE_SIZE)
 
